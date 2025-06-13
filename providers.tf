f68f2387a94d7e22ebba0840c37e17c07044a556
Kamenev Vladimir <fasca23@yandex.ru>
2025-06-13 14:00:07 +0300

    Исправление_4

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
   required_version = ">=1.9"
 
   backend "s3" {
-    endpoint   = "https://storage.yandexcloud.net"
+    s3         = "https://storage.yandexcloud.net"
     bucket     = var.s3_bucket_name
     region     = "ru-central1"
     key        = "terraform.tfstate"
