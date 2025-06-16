7cd9a165cddb9894117d2ac29c4d1eabc992c7be
Kamenev Vladimir <fasca23@yandex.ru>
2025-06-16 11:10:23 +0300

    Исправление_11

--- ansible.tf
+++ ansible.tf
@@ -7,7 +7,7 @@ resource "local_file" "kubespray_inventory" {
     worker_nodes = slice(yandex_compute_instance.cluster-k8s, 1, length(yandex_compute_instance.cluster-k8s)) # Остальные — воркеры
   })
 
-  filename = "../k8s-spray/kubespray/inventory/hosts.yaml"
-
+  # filename = "../k8s-spray/kubespray/inventory/hosts.yaml"
+    filename = "${path.module}/kubespray/inventory/hosts.yaml"
   depends_on = [yandex_compute_instance.cluster-k8s]
 }
